dde80ba425ab8bcf5b885c1e607bcea3736b3637
haita <haita-email@example.com>
2023-12-02 10:33:45 +0700

    cập nhật

--- home.php
+++ home.php
@@ -4,23 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>home</title>
-    <!--fonts awesome cdn link-->
+    <!-- font awesome cdn link  -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-    <!--custom css file link-->
+    <!-- custom css file link  -->
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
     <div class="container">
-    <?php @include 'header.php'; ?>
+    <?php @include 'header.php'?>
     </div>
-
-
-
-
-
-
-
-    <!--custom js file link-->
+    <!-- custom js file link  -->
     <script src="js/script.js"></script>
 </body>
 </html>
